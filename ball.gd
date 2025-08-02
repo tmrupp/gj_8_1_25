e4a2abc6934d92758cde1da3506fbfe890ca04eb
tmrupp <tmrupp@umich.edu>
2025-08-01 19:40:14 -0700

    Balls clank

--- ball.gd
+++ ball.gd
@@ -2,34 +2,44 @@ extends RigidBody3D
 
 @onready var collider = $Collider
 
+@onready var hit_point = $"../HitPoint"
+@export var is_cue = true
+@export var friction = 100.0
+
 func _ready():
 	connect("body_entered", collision)
 	#apply_impulse(Vector3.FORWARD*1200)
 	
 func _process(delta):
-	print("position=", position)
+	#print("position=", position)
 	pass
+	
+func _physics_process(delta):
+	apply_force(-linear_velocity.normalized()*friction*delta)
 	
 func collision(_other):
 	# Get body rid
 	var rid := get_rid()
 	# Get body physics state
 	var state := PhysicsServer3D.body_get_direct_state(rid)
+	print("collision, self=", self, " other=", _other)
 	hit(state.get_contact_local_position(0))
 
 func hit (point):
-	var direction = point - position
+	var direction = position - point
 	print("hit @ ", point, " from ", position, " direction ", direction)
-	apply_impulse(direction.normalized()*100)
+	apply_impulse(direction.normalized()*10)
 	print(linear_velocity)
 	
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		var mouse_pos = get_viewport().get_mouse_position()
+	if is_cue and event is InputEventMouseButton and event.is_pressed():
 		var space_state = get_world_3d().direct_space_state
 		# use global coordinates, not local to node
-		var query = PhysicsRayQueryParameters3D.create(Vector3(mouse_pos.x, 0, mouse_pos.y), position)
+		print("global_position=", global_position)
+		var query = PhysicsRayQueryParameters3D.create(hit_point.global_position, global_position)
 		var result = space_state.intersect_ray(query)
 		if "position" in result:
 			hit(result["position"])
-		print("Mouse clicked at: ", mouse_pos, " - Applying force to body")
+		else:
+			print("No hit detected")
+		print("Mouse clicked at: ", hit_point.global_position, " - Applying force to body")
