a94987d2375a2164322e62982fed8b808b82f4d1
nichgaun <nichgaun@umich.edu>
2025-08-02 19:41:31 -0400

    Remove extraneous test input, add pocket walls to prevent map exiting

--- cue.gd
+++ cue.gd
@@ -8,13 +8,11 @@ var remaining_cooldown_time = 0
 func _process(delta: float) -> void:
 	if remaining_cooldown_time > 0:
 		remaining_cooldown_time -= delta
-	if Input.is_action_just_pressed("test_input"):
-		print("in direction: ", global_basis.x)
 
 func shoot():
 	if ray_cast_3d.is_colliding() and not remaining_cooldown_time > 0:
 		var collider = ray_cast_3d.get_collider()
 		if collider.get_node("..").is_cue:
-			print("cue hit against: ", collider.get_node("..").name, " in direction: ", global_basis.x)
+			#print("cue hit against: ", collider.get_node("..").name, " in direction: ", global_basis.x)
 			collider.get_node("..").hit_via_vector(global_basis.x)
 			remaining_cooldown_time = max_cooldown_time
