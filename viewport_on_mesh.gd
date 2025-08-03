515fc0a7e3f6a2f2e888048e9abc79c243d89e7f
nichgaun <nichgaun@umich.edu>
2025-08-03 15:30:02 -0400

    Fixing player animation, adding first pass of music

--- viewport_on_mesh.gd
+++ viewport_on_mesh.gd
@@ -1,8 +1,12 @@
 extends MeshInstance3D
 
 @onready var sub_viewport: SubViewport = $"../SubViewport"
+@onready var camera_3d: Camera3D = $"../ThirdPersonLocation/Camera3D"
 
 func _ready() -> void:
-	sub_viewport.set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
+	sub_viewport.set_clear_mode(SubViewport.CLEAR_MODE_ALWAYS)
 	
 	material_override.albedo_texture = sub_viewport.get_texture()
+
+func _process(delta: float) -> void:
+	rotation.x = camera_3d.rotation.x
